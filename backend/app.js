0da8938c9273c93ec02d72fc1be72e85c3378b9a
Ifeoluwa <ifel1939@gmail.com>
2023-08-03 15:30:01 +0100

    handling errors

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,30 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connectDB");
+require("dotenv").config();
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorHandler");
+const port = process.env.PORT || 5000;
+const authRoute = require("./routes/authRouter");
+const userRoute = require("./routes/userRoute");
 
-require("dotenv").config()
-const notFound=require("./middleware/notFound")
-const errorHandler=require("./middleware/errorHandler")
-const port=process.env.PORT || 5000
-const authRoute=require("./routes/authRouter")
-const userRoute=require('./routes/userRoute')
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("<h1> Converting CV<h1>");
+});
 
-
-app.use(express.json())
-
-
-
-
-app.get("/",(req,res)=>{
-    res.send("<h1> Converting CV<h1>")
-})
-
-app.use("/api/v1/auth",authRoute)
-app.use("/api/v1/users/",userRoute)
-
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users/", userRoute);
 
 app.use(notFound);
-app.use(errorHandler)
+app.use(errorHandler);
 
+const start = async () => {
+  await connectDB(process.env.MONGO_URL);
+  app.listen(process.env.PORT, () => {
+    console.log(`server listening on port ${port}`);
+  });
+};
 
-
-
-
-
-app.listen(process.env.PORT,()=>{console.log(`server listening on port ${port}`)})
+start()
