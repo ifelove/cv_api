29bff02b549d23b3dd1a7488d8d35a9e0edb9df4
Ifeoluwa <ifel1939@gmail.com>
2023-08-16 09:10:51 +0100

    min lenght removed

--- backend/model/Resume.js
+++ backend/model/Resume.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 const ResumeSchema = new mongoose.Schema({
   firstname: { type: String },
   othername: { type: String },
-  surname:{type:String},
-  profSummary:{type:String},
-  phoneNo:{type:String},
-  email:{}
+  surname: { type: String },
+  profSummary: { type: String },
+  phoneNo: { type: Number },
+  email: {
+    type: String,
+    validate: {
+      validator: validator.isEmail,
+      msg: "Email is Invalid",
+    },
+  },
+  education:{type : [String]}
 });
 
 module.exports = mongoose.model("Resume", ResumeSchema);
--- backend/model/User.js
+++ backend/model/User.js
@@ -5,7 +5,6 @@ const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please provide name"],
-    minlength: 3,
     maxlength: 70,
   },
   email: {
